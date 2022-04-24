8e125b3a9de17bdc2e0416b7cbc4550a577cac98
faiqnoov <mfaiqn@gmail.com>
2022-04-24 22:01:46 +0700

    create frontend 5 halaman inti

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        color1: '#00296B',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
